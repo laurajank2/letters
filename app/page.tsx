08479a2c5b2b077364cb519a5316e310b1f0651d
unknown <laura_jankowski@brown.edu>
2024-07-11 14:33:11 -0700

    changed title style

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Gameplay from "./ui/gameplay";
 export default function Home() {
   return (
     <main className="app">
-      <h1>Letters</h1>
+      <h1>LETTERS</h1>
       <Gameplay/>
     </main>
   );
